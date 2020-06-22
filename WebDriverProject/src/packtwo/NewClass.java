3154b7fa41389402cfe97e873d5e69ee21435f6d
52094750 <52094750@CTSHYD52094750.acsind.com>
2020-06-22 10:23:10 +0530

    C3

--- WebDriverProject/src/packtwo/NewClass.java
+++ WebDriverProject/src/packtwo/NewClass.java
@@ -5,6 +5,8 @@ public class NewClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("This is new Code");
+
+System.out.println("These are the changes");
 	}
 
 }
